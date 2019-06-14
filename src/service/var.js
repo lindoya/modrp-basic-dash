eb4a112171009de59a163e29234382878804d49f
cintiatilly <joannisreal@gmail.com>
2019-06-14 14:02:15 -0300

    serivodor2019-06-14

--- src/service/var.js
+++ src/service/var.js
@@ -1,3 +1,3 @@
-// export const urlBackEnd = 'http://200.196.235.175:5312'
-export const urlBackEnd = 'http://192.168.0.174:5312'
+export const urlBackEnd = 'http://200.196.235.175:5312'
+// export const urlBackEnd = 'http://192.168.0.174:5312'
 // export const urlBackEnd = 'http://localhost:5312'
